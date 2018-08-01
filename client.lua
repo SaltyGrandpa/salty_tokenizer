6d6e0d08bc2564663a983591c4062572eb0bf598
Corey McCown <coreymccown13@gmail.com>
2018-08-01 16:12:23 -0400

    Update client.lua
    Add support for unique obfuscated token listeners through export functions.

--- client.lua
+++ client.lua
@@ -1,8 +1,49 @@
+local waiting = false
 local firstSpawn = true
+local resourceNames = {}
+
+RegisterNetEvent("salty_tokenizer:obfuscateReceived")
+AddEventHandler("salty_tokenizer:obfuscateReceived", function(resource, name)
+	resourceNames[resource] = name
+	waiting = false
+end)
+
+function init()	
+	Citizen.CreateThread(function()
+		TriggerEvent('salty_tokenizer:clientReady')
+	end)
+	Citizen.CreateThread(function()
+		Citizen.Wait(500)
+		TriggerServerEvent('salty_tokenizer:playerSpawned')
+	end)
+end
 
 AddEventHandler("playerSpawned", function()
 	if firstSpawn then
 		firstSpawn = false
-		TriggerServerEvent('salty_tokenizer:requestToken')
+		init()
 	end
 end)
+
+function requestObfuscatedEventName(resource)
+	if resourceNames[resource] == nil then
+		TriggerServerEvent('salty_tokenizer:requestObfuscation', resource)
+		waiting = true
+		while waiting do
+			Citizen.Wait(0)
+		end
+	end
+	return resourceNames[resource]
+end
+
+function setupClientResource(resource)
+	local tempToken = false
+	RegisterNetEvent(requestObfuscatedEventName(resource))
+	AddEventHandler(requestObfuscatedEventName(resource), function(token)
+		tempToken = token
+	end)
+	while not tempToken do
+		Citizen.Wait(0)
+	end
+	return tempToken
+end
