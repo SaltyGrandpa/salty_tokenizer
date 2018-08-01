3d6139ec96d732f7c636f0061a7232f977de5f79
Corey McCown <coreymccown13@gmail.com>
2018-08-01 16:11:42 -0400

    Update __resource.lua
    Setup exports for new token listeners.

--- __resource.lua
+++ __resource.lua
@@ -2,11 +2,18 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Add security tokens to server events.'
 
+client_script 'client.lua'
+
 server_scripts {
 	'config.lua',
 	'server.lua'
 }
 
-client_scripts {
-	'client.lua'
+exports {
+	'setupClientResource'
+}
+
+server_exports {
+	'setupServerResource',
+	'secureServerEvent'
 }
