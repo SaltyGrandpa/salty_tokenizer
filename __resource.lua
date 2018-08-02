9bef807488cfcbd059186d26b2ff4984011a7b07
Corey McCown <coreymccown13@gmail.com>
2018-08-02 10:54:57 -0400

    Update __resource.lua
    Add config to client for verbosity options.

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,10 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Add security tokens to server events.'
 
-client_script 'client.lua'
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
 
 server_scripts {
 	'config.lua',
