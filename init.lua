97bed496e1a9c497bcdaaa5a53af04a0384b5cfd
Corey McCown <coreymccown13@gmail.com>
2018-09-05 05:31:42 -0400

    Add init script
    Add init script that can be included in __resource.lua on client and server side to initialize the tokenizer.

---
+++ init.lua
@@ -0,0 +1,10 @@
+if IsDuplicityVersion() then
+	AddEventHandler('salty_tokenizer:clientReady', function()
+		securityToken = exports['salty_tokenizer']:setupClientResource(GetCurrentResourceName())
+	end)
+else
+	securityToken = nil
+	AddEventHandler('salty_tokenizer:serverReady', function()
+		exports['salty_tokenizer']:setupServerResource(GetCurrentResourceName())
+	end)
+end
