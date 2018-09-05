86c3e1073e03f1855f03a16b12f8fbc0294a48c0
Corey McCown <coreymccown13@gmail.com>
2018-09-05 05:57:49 -0400

    Update init.lua
    Fixed duplicity logic... haven't had coffee yet.

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 if IsDuplicityVersion() then
-	AddEventHandler('salty_tokenizer:clientReady', function()
-		securityToken = exports['salty_tokenizer']:setupClientResource(GetCurrentResourceName())
+	AddEventHandler('salty_tokenizer:serverReady', function()
+		exports['salty_tokenizer']:setupServerResource(GetCurrentResourceName())
 	end)
 else
 	securityToken = nil
-	AddEventHandler('salty_tokenizer:serverReady', function()
-		exports['salty_tokenizer']:setupServerResource(GetCurrentResourceName())
+	AddEventHandler('salty_tokenizer:clientReady', function()
+		securityToken = exports['salty_tokenizer']:setupClientResource(GetCurrentResourceName())
 	end)
 end
