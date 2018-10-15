125d27dba054136d071944afccae08946f6f0c62
Corey McCown <coreymccown13@gmail.com>
2018-10-15 16:33:16 -0400

    Update config.lua
    Adding custom action handling

--- config.lua
+++ config.lua
@@ -42,3 +42,13 @@ Config.ClientDelay = 250
 	Define the message given to users with an invalid token
 --]]
 Config.KickMessage = "Invalid security token detected."
+
+--[[
+	Define a custom function to trigger when a player is kicked
+	If Config.CustomAction is false, the player will be dropped
+]]
+Config.CustomAction = false
+Config.CustomActionFunction = function(source)
+	print("Custom action executing for: " .. source)
+	DropPlayer(source, Config.KickMessage)
+end
